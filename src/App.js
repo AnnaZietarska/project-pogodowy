e4e3ebd8f4dec272ac621a1bc9acf84eaeeaf579
AnnaZietarska <anna.zietarska@gmail.com>
2021-10-11 10:28:04 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Create React App</div>;
+  return (
+    <div className="App">
+      <h1>Create React App</h1>
+      <footer>
+        This project was coded by Anna Ziętarska and is{" "}
+        <a
+          href="https://github.com/AnnaZietarska/project-pogodowy"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
